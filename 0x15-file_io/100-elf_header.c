63b6024886dc6bc1b6363e55903cbc0f8a02f416
Akinremi Isaiah Adebayo <akinremi93@gmail.com>
2022-10-31 20:21:53 -0700

    ok

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -180,8 +180,8 @@ void print_osabi(unsigned char *e_ident)
  */
 
 void print_abi(unsigned char *e_ident)
-
 {
+
 	printf("  ABI Version:                       %d\n",
 	       e_ident[EI_ABIVERSION]);
 }
@@ -192,6 +192,7 @@ void print_abi(unsigned char *e_ident)
  */
 void print_type(unsigned int e_type, unsigned char *e_ident)
 {
+
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
@@ -223,8 +224,8 @@ void print_type(unsigned int e_type, unsigned char *e_ident)
  * @e_ident: A pointer to an array containing the ELF class.
  */
 void print_entry(unsigned long int e_entry, unsigned char *e_ident)
-
 {
+
 	printf("  Entry point address:               ");
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
@@ -246,6 +247,7 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident)
  */
 void close_elf(int elf)
 {
+
 	if (close(elf) == -1)
 	{
 		dprintf(STDERR_FILENO,
@@ -264,12 +266,13 @@ void close_elf(int elf)
  * Description: If the file is not an ELF File or
  *              the function fails - exit code 98.
  */
-
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
+
 	Elf64_Ehdr *header;
 	int o, r;
 	o = open(argv[1], O_RDONLY);
+
 	if (o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
